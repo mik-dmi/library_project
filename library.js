b70f097fe67a7191d7c63c141f0b995995cc80f3
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-08-14 19:28:33 +0100

    It has a but that displays a forms, and when the forms is complete it hides and the button to make appear the forms appeares again

--- library.js
+++ library.js
@@ -2,5 +2,36 @@ function addBookToLibrary() {
     // do stuff here
   }
 
+  function Book() {
 
+    
+  }
+
+  
+  
   let myLibrary = [];
+
+  const showFormsButton = document.getElementById("addBookButton");
+  const forms = document.querySelector("form");
+  const nameOfBook = forms["name"];
+  const author = forms["author"];
+  const pages = forms["pages"];
+  const check = forms["read_or_not"]
+  const addBookButton = document.getElementById('.create_a_book_card');
+
+  showFormsButton.addEventListener("click", ()=>{
+  forms.style.display = "flex";
+  showFormsButton.style.display = "none"; 
+  })
+
+  forms.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const book = new Book()
+    forms.style.display = "none";   /*hides the forms*/
+    showFormsButton.style.display = "block";  /*show the + button again"*/
+
+    forms.reset();  /*clear the form for the next time*/
+  }
+  
+  )
+
